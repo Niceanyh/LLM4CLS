d600ff2a4533ebf6790616e7f6efb29a337b3b00
Niceanyh <niceanyh@gmail.com>
2023-08-27 12:16:04 +0100

    tailor fix bug

--- llm4cls/util.py
+++ llm4cls/util.py
@@ -40,9 +40,9 @@ def few_shot_prompt_builder(task_description,query,samples,label2text,tailor_siz
     else:
         # tailor input size
         input = tailer(query,tailor_size)
-        demos = "\n".join([f"<Example {i+1}>: {tailer(samples[i]['text'])} <Answer {i+1}>: {label2text(samples[i]['label'])}"
+        demos = "\n".join([f"<Example {i+1}>: {tailer(samples[i]['text'],tailor_size)} <Answer {i+1}>: {label2text(samples[i]['label'])}"
                               for i in range(len(samples))])
-        
+        print(task_description + demos + "\n"+ " <Input>:  " + input + " <Answer>:")
         return task_description + demos + "\n"+ " <Input>:  " + input + " <Answer>:"
 
 def tailer(query,tailor_size):
