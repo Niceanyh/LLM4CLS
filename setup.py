6d5fc435a0cdf0ed6937b32d27bc2565bca76117
Niceanyh <niceanyh@gmail.com>
2023-08-27 12:01:11 +0100

    version0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='llm4cls',
-    version='0.1',
+    version='0.2',
     description='Text Classification with Large Language Models',
     author='niceanyh',
     author_email='niceanyh@gmail.com',
